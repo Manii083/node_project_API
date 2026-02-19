290404dade1e64b03d1160c0eab28b5211e8b112
Manideep Katkam <manideepkatkam83@gmail.com>
2026-02-19 14:08:07 +0530

    Remove sensitive information from db.js
    Removed sensitive database connection string.

--- db.js
+++ db.js
@@ -1,6 +1,4 @@
-// VqTz6QRkuhnVbwx7
 
-// mongodb+srv://manideepkatkam83_db_user:VqTz6QRkuhnVbwx7@cluster0.uem8mvd.mongodb.net/?appName=Cluster0
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
